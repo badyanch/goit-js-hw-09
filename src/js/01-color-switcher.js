b8f3cacf02acf208e874751cfcc81de95012e4bd
Bohdan Zlotko <bohdanzlotko@gmail.com>
2023-04-14 17:59:23 +0300

    fix

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,36 @@
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+let intervalEl = null;
 
+btnStop.setAttribute('disabled', '');
+
+btnStart.addEventListener('click', onButtonStart);
+btnStop.addEventListener('click', onButtonStop);
+
+function onButtonStart() {
+  onToggelBtnActivity();
+  changeColorBody();
+
+  intervalEl = setInterval(changeColorBody, 1000);
+}
+
+function onButtonStop() {
+  clearInterval(intervalEl);
+
+  onToggelBtnActivity();
+}
+
+function changeColorBody() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+
+function onToggelBtnActivity() {
+  btnStart.toggleAttribute('disabled');
+  btnStop.toggleAttribute('disabled');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+  }
